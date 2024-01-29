de196189fe33448b424e39f40670850b15ac4ce9
Teddy Nguyen <teddy.nguyen@techvify.com.vn>
2024-01-29 23:22:04 +0700

    Read CSV file and get column header in FE

--- frontend/src/components/UploadFile.vue
+++ frontend/src/components/UploadFile.vue
@@ -1,48 +1,30 @@
 <template>
   <div>
     <v-file-input label="File input" @change.native="upload"></v-file-input>
-    <v-select label="Select" :items="[
-      'California',
-      'Colorado',
-      'Florida',
-      'Georgia',
-      'Texas',
-      'Wyoming',
-    ]"></v-select>
+    <v-select label="Select" :items="csvHeader"></v-select>
   </div>
 </template>
 
 <script setup>
 import { ref, onMounted, defineEmits } from "vue";
+import Papa from 'papaparse';
 
 const emits = defineEmits(["file-uploaded"]);
 
 const selectedFile = ref(null);
 const uploadStatus = ref("");
-
-// const handleFileUpload = (event) => {
-//   const file = event.target.files[0];
-
-//   if (file && file.type === "text/csv") {
-//     selectedFile.value = file;
-//   } else {
-//     // Reset selectedFile if the file is not a CSV
-//     selectedFile.value = null;
-//     alert("Please upload a valid CSV file.");
-//   }
-// };
+const csvHeader = ref([])
 
 const upload = async (e) => {
-  console.log(e);
   selectedFile.value = e.target.files[0]
   if (!selectedFile.value) {
     alert("Please select a file before uploading.");
     return;
   }
-
+  readCsv(selectedFile.value)
   const formData = new FormData();
   formData.append("file", selectedFile.value);
-  console.log(formData);
+
   try {
     const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/upload`, {
       method: "POST",
@@ -63,7 +45,22 @@ const upload = async (e) => {
   }
 };
 
-// Clear selectedFile when the component is mounted
+const readCsv = (file) => {
+  Papa.parse(file, {
+    header: true,
+    dynamicTyping: true,
+    complete: (results) => {
+      if (results.data.length > 0) {
+        csvHeader.value = Object.keys(results.data[0]);
+      }
+    },
+    error: (error) => {
+      console.error('CSV Parsing Error:', error.message);
+    },
+  });
+}
+
+
 onMounted(() => {
   selectedFile.value = null;
   uploadStatus.value = "";
